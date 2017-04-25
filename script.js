53effca51c893c37b4a7afcd19487653660fd00c
Rori Kotch <r.kotch1@umiami.edu>
2017-04-25 13:40:34 -0400

    Fixed highchairs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Highcharts.chart("container", {
 
     title: {
-        text: "U.S. Milk Production 1998 to 2017"
+        text: "U.S. Milk Production 2010 to 2017"
     },
 
     subtitle: {
@@ -11,18 +11,18 @@ Highcharts.chart("container", {
 
     yAxis: {
         title: {
-            text: "Billions of Pounds"
+            text: "Millions of Pounds"
         }
     },
 
     plotOptions: {
         series: {
-            pointStart: 1998
+            pointStart: 2010
         }
     },
 
-    series: {
+    series: [{
         name: "Production",
-        data: [16123000000, 16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
-    },
+        data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
+    }]
   })
