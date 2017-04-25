d9ebd83f5761c1db62b65e9b0b442e01b2680395
Rori Kotch <r.kotch1@umiami.edu>
2017-04-24 23:49:53 -0400

    Added script
    Tried adding highcharts into html

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 Highcharts.chart('container', {
 
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'U.S. Milk Production 1998 to 2017'
     },
 
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+        text: 'This data was sourced from the U.S.D.A. Department of Agriculture'
     },
 
     yAxis: {
         title: {
-            text: 'Number of Employees'
+            text: 'Billions of Pounds'
         }
     },
     legend: {
@@ -21,25 +21,12 @@ Highcharts.chart('container', {
 
     plotOptions: {
         series: {
-            pointStart: 2010
+            pointStart: 1998
         }
     },
 
     series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }]
-
+        name: 'Production',
+        data: [16123000000, 16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
+    },
 });
