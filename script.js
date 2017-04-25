f9a8f67dd4c00b1120f2c7f811bc19a0f9f968fa
Rori Kotch <r.kotch1@umiami.edu>
2017-04-25 00:04:40 -0400

    Double quotes

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-Highcharts.chart('container', {
+$(function() {
+  var myChart = Highcharts.chart("container", {
 
     title: {
-        text: 'U.S. Milk Production 1998 to 2017'
+        text: "U.S. Milk Production 1998 to 2017"
     },
 
     subtitle: {
-        text: 'This data was sourced from the U.S.D.A. Department of Agriculture'
+        text: "This data was sourced from the U.S.D.A. Department of Agriculture"
     },
 
     yAxis: {
         title: {
-            text: 'Billions of Pounds'
+            text: "Billions of Pounds"
         }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
     },
 
     plotOptions: {
@@ -26,7 +22,7 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        name: 'Production',
+        name: "Production",
         data: [16123000000, 16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
     },
 });
